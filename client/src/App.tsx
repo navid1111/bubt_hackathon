4ee5793551ea724bb17fded96612d2de28c15110
SadLabib <aslabib44@gmail.com>
2025-11-20 22:08:12 +0600

    Added sidebar and layout.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,33 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
 import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import OnboardingPage from './pages/OnboardingPage';
+import { ProfileProvider } from './context/ProfileContext';
+
+// Layout
+import Layout from './components/Layout';
+
+// Pages with Layout
+import Dashboard from './pages/Dashboard';
+import DailyLogPage from './pages/DailyLogPage';
+// import InventoryPage from './pages/InventoryPage';
+// import ResourcesPage from './pages/ResourcesPage';
+// import NeighbourhoodPage from './pages/NeighbourhoodPage';
 import ProfilePage from './pages/ProfilePage';
 import EditProfilePage from './pages/EditProfilePage';
-import { ProfileProvider } from './context/ProfileContext';
 
 export default function App() {
   return (
     <ProfileProvider>
       <Routes>
-        {/* Public routes */}
+        {/* Public routes (no layout) */}
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
-
-        {/* Protected routes */}
+        
+        {/* Onboarding (no layout) */}
         <Route
           path="/onboarding"
           element={
@@ -27,30 +36,24 @@ export default function App() {
             </SignedIn>
           }
         />
+
+        {/* Protected routes with layout */}
         <Route
-          path="/dashboard"
+          path="/"
           element={
             <SignedIn>
-              <Dashboard />
+              <Layout />
             </SignedIn>
           }
-        />
-        <Route
-          path="/profile"
-          element={
-            <SignedIn>
-              <ProfilePage />
-            </SignedIn>
-          }
-        />
-        <Route
-          path="/profile/edit"
-          element={
-            <SignedIn>
-              <EditProfilePage />
-            </SignedIn>
-          }
-        />
+        >
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="daily-log" element={<DailyLogPage />} />
+          {/* <Route path="inventory" element={<InventoryPage />} />
+          <Route path="resources" element={<ResourcesPage />} />
+          <Route path="neighbourhood" element={<NeighbourhoodPage />} /> */}
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="profile/edit" element={<EditProfilePage />} />
+        </Route>
 
         {/* Redirect to sign in when signed out */}
         <Route
