bdbed3837e4fb81a994df4c2e5509e128d984724
SadLabib <aslabib44@gmail.com>
2025-11-21 13:02:51 +0600

    feat(inventory): add Utensils icon import for inventory detail page

--- client/src/pages/InventoryDetailPage.tsx
+++ client/src/pages/InventoryDetailPage.tsx
@@ -14,6 +14,7 @@ import {
   FileText,
   Trash2,
   Camera,
+  Utensils,
 } from 'lucide-react';
 import { useInventory } from '../hooks/useInventory';
 import ImageUploadModal from '../components/inventory/ImageUploadModal';
